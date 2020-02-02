ac717fdc1368d6338cbb77d37f04b06235d1b937
Tiago <Tiago Santos>
2020-02-02 17:01:08 +1300

    React advanced course

---
+++ src/actions/actionCreator.js
@@ -0,0 +1,16 @@
+export function listagem(fotos){
+    return {type:'LISTAGEM', fotos};
+}
+
+export function comentario(fotoId, novoComentario){
+    return {type:'COMENTARIO', fotoId, novoComentario};
+}
+
+export function like(fotoId, liker){
+    return {type:'LIKE', fotoId, liker};
+}
+
+export function notifica(msg){
+    return {type:'ALERT', msg};
+}
+
